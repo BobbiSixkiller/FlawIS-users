7a27334e1580d66e1088f7f59feef20bfd82bbc2
Matus Muransky <matus.muransky2@gmail.com>
2022-07-11 12:23:49 +0200

    user role refactored to use enum and permissions removed

--- api/entitites/User.ts
+++ api/entitites/User.ts
@@ -1,184 +1,191 @@
 import {
-	ArgumentValidationError,
-	Directive,
-	Field,
-	ObjectType,
+  ArgumentValidationError,
+  Directive,
+  Field,
+  ObjectType,
+  registerEnumType,
 } from "type-graphql";
 import { ObjectId } from "mongodb";
 import {
-	getModelForClass,
-	Index,
-	pre,
-	prop as Property,
+  getModelForClass,
+  Index,
+  pre,
+  prop as Property,
 } from "@typegoose/typegoose";
 import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import { hash } from "bcrypt";
 import { signJwt } from "../util/auth";
 import CreateConnection from "../resolvers/types/pagination";
 
+enum Role {
+  Basic = "BASIC",
+  Admin = "ADMIN",
+}
+
+registerEnumType(Role, {
+  name: "Role", // this one is mandatory
+  description: "User role inside the FLAWIS system", // this one is optional
+});
+
 @ObjectType()
 export class Address {
-	@Field()
-	@Property()
-	street: string;
+  @Field()
+  @Property()
+  street: string;
 
-	@Field()
-	@Property()
-	city: string;
+  @Field()
+  @Property()
+  city: string;
 
-	@Field()
-	@Property()
-	postal: string;
+  @Field()
+  @Property()
+  postal: string;
 
-	@Field()
-	@Property()
-	country: string;
+  @Field()
+  @Property()
+  country: string;
 }
 
 @ObjectType({ description: "User's billing information" })
 export class Billing {
-	@Field({ nullable: true })
-	id?: ObjectId;
+  @Field({ nullable: true })
+  id?: ObjectId;
 
-	@Field()
-	@Property()
-	name: string;
+  @Field()
+  @Property()
+  name: string;
 
-	@Field(() => Address)
-	@Property()
-	address: Address;
+  @Field(() => Address)
+  @Property()
+  address: Address;
 
-	@Field()
-	@Property()
-	ICO: string;
+  @Field()
+  @Property()
+  ICO: string;
 
-	@Field()
-	@Property()
-	DIC: string;
+  @Field()
+  @Property()
+  DIC: string;
 
-	@Field()
-	@Property()
-	ICDPH: string;
+  @Field()
+  @Property()
+  ICDPH: string;
 
-	@Field({ nullable: true })
-	@Property()
-	IBAN?: string;
+  @Field({ nullable: true })
+  @Property()
+  IBAN?: string;
 
-	@Field({ nullable: true })
-	@Property()
-	SWIFT?: string;
+  @Field({ nullable: true })
+  @Property()
+  SWIFT?: string;
 }
 
 @pre<User>("save", async function () {
-	if (this.isNew || this.isModified("password")) {
-		this.password = await hash(this.password, 12);
-	}
-	if (this.isNew || this.isModified("email")) {
-		const emailExists = await getModelForClass(User)
-			.findOne({ email: this.email })
-			.exec();
-		if (emailExists && emailExists.id !== this.id) {
-			throw new ArgumentValidationError([
-				{
-					target: User, // Object that was validated.
-					property: "email", // Object's property that haven't pass validation.
-					value: this.email, // Value that haven't pass a validation.
-					constraints: {
-						// Constraints that failed validation with error messages.
-						EmailExists: "Submitted email address is already in use!",
-					},
-					//children?: ValidationError[], // Contains all nested validation errors of the property
-				},
-			]);
-		}
-	}
-	if (this.isNew && (this.isFlaw || this.isUniba)) {
-		this.billings.push({
-			name: "Univerzita Komenského v Bratislave, Právnická fakulta",
-			ICO: "00397865",
-			DIC: "2020845332",
-			ICDPH: "SK2020845332",
-			address: {
-				street: "Šafárikovo nám. 6",
-				city: "Bratislava",
-				postal: "81000",
-				country: "Slovensko",
-			},
-		});
-	}
+  if (this.isNew || this.isModified("password")) {
+    this.password = await hash(this.password, 12);
+  }
+  if (this.isNew || this.isModified("email")) {
+    const emailExists = await getModelForClass(User)
+      .findOne({ email: this.email })
+      .exec();
+    if (emailExists && emailExists.id !== this.id) {
+      throw new ArgumentValidationError([
+        {
+          target: User, // Object that was validated.
+          property: "email", // Object's property that haven't pass validation.
+          value: this.email, // Value that haven't pass a validation.
+          constraints: {
+            // Constraints that failed validation with error messages.
+            EmailExists: "Submitted email address is already in use!",
+          },
+          //children?: ValidationError[], // Contains all nested validation errors of the property
+        },
+      ]);
+    }
+  }
+  if (this.isNew && (this.isFlaw || this.isUniba)) {
+    this.billings.push({
+      name: "Univerzita Komenského v Bratislave, Právnická fakulta",
+      ICO: "00397865",
+      DIC: "2020845332",
+      ICDPH: "SK2020845332",
+      address: {
+        street: "Šafárikovo nám. 6",
+        city: "Bratislava",
+        postal: "81000",
+        country: "Slovensko",
+      },
+    });
+  }
 })
 @Index({ name: "text" })
 @Directive('@key(fields: "id")')
 @ObjectType({ description: "The user model entity" })
 export class User extends TimeStamps {
-	@Field()
-	id: ObjectId;
+  @Field()
+  id: ObjectId;
 
-	@Field()
-	@Property()
-	email: string;
+  @Field()
+  @Property()
+  email: string;
 
-	@Property()
-	password: string;
+  @Property()
+  password: string;
 
-	@Field()
-	@Property({ default: "BASIC", enum: ["BASIC", "SUPERVISOR", "ADMIN"] })
-	role: string;
+  @Field(() => Role)
+  @Property({ default: "BASIC", enum: ["BASIC", "SUPERVISOR", "ADMIN"] })
+  role: Role;
 
-	@Field(() => [String])
-	@Property({
-		type: () => [String],
-		default: [],
-	})
-	permissions: string[];
+  @Field()
+  @Property()
+  name: string;
 
-	@Field()
-	@Property()
-	name: string;
+  @Field({ nullable: true })
+  @Property()
+  telephone?: string;
 
-	@Field({ nullable: true })
-	@Property()
-	telephone?: string;
+  @Field()
+  @Property()
+  organisation: string;
 
-	@Field()
-	@Property()
-	organisation: string;
+  @Field(() => [Billing])
+  @Property({ type: () => [Billing], default: [] })
+  billings: Billing[];
 
-	@Field(() => [Billing])
-	@Property({ type: () => [Billing], default: [] })
-	billings: Billing[];
+  @Field()
+  @Property({ default: false })
+  active: boolean;
 
-	@Field()
-	createdAt: Date;
-	@Field()
-	updatedAt: Date;
+  @Field()
+  createdAt: Date;
+  @Field()
+  updatedAt: Date;
 
-	get token(): string {
-		return (
-			"Bearer " +
-			signJwt(
-				{
-					id: this.id,
-					email: this.email,
-					name: this.name,
-					role: this.role,
-					permissions: this.permissions,
-				},
-				{ expiresIn: "7d" }
-			)
-		);
-	}
+  get token(): string {
+    return (
+      "Bearer " +
+      signJwt(
+        {
+          id: this.id,
+          email: this.email,
+          name: this.name,
+          role: this.role,
+        },
+        { expiresIn: "7d" }
+      )
+    );
+  }
 
-	get isFlaw(): boolean {
-		return this.email.split("@")[1] === "flaw.uniba.sk";
-	}
+  get isFlaw(): boolean {
+    return this.email.split("@")[1] === "flaw.uniba.sk";
+  }
 
-	get isUniba(): boolean {
-		return this.email.split("@")[1] === "uniba.sk";
-	}
+  get isUniba(): boolean {
+    return this.email.split("@")[1] === "uniba.sk";
+  }
 }
 
 @ObjectType({
-	description: "UserConnection type enabling cursor based pagination",
+  description: "UserConnection type enabling cursor based pagination",
 })
 export class UserConnection extends CreateConnection(User) {}
--- api/resolvers/user.ts
+++ api/resolvers/user.ts
@@ -5,11 +5,11 @@ import { User, UserConnection } from "../entitites/User";
 import { CRUDservice } from "../services/CRUDservice";
 import { Mutation } from "type-graphql";
 import {
-	BillingInput,
-	PasswordInput,
-	RegisterInput,
-	UserArgs,
-	UserInput,
+  BillingInput,
+  PasswordInput,
+  RegisterInput,
+  UserArgs,
+  UserInput,
 } from "./types/user";
 import { transformIds } from "../util/typegoose-middleware";
 import { AuthenticationError, UserInputError } from "apollo-server-core";
@@ -23,203 +23,204 @@ import { ResetToken } from "../util/types";
 @Service()
 @Resolver()
 export class UserResolver {
-	constructor(private readonly userService = new CRUDservice(User)) {}
+  constructor(private readonly userService = new CRUDservice(User)) {}
 
-	@Authorized(["ADMIN", "SUPERVISOR", "IS_OWN_USER"])
-	@Query(() => User)
-	async user(@Arg("id") id: ObjectId): Promise<User> {
-		const user = await this.userService.findOne({ _id: id });
-		if (!user) throw new Error("User not found!");
+  @Authorized(["ADMIN", "IS_OWN_USER"])
+  @Query(() => User)
+  async user(@Arg("id") id: ObjectId): Promise<User> {
+    const user = await this.userService.findOne({ _id: id });
+    if (!user) throw new Error("User not found!");
 
-		return user;
-	}
+    return user;
+  }
 
-	@Authorized("ADMIN", "SUPERVISOR")
-	@Query(() => UserConnection)
-	async users(
-		@Args() { first, after, last, before }: UserArgs
-	): Promise<UserConnection> {
-		const users = await this.userService.aggregate([
-			{
-				$match: {
-					$expr: {
-						$cond: [
-							{ $and: [{ $eq: [before, null] }, { $eq: [after, null] }] },
-							{ $ne: ["$_id", null] }, //return all sorted documents
-							{
-								$cond: [
-									{ $eq: [before, null] },
-									{ $lt: ["$_id", new ObjectId(after)] }, //newer users
-									{ $gt: ["$_id", new ObjectId(before)] }, //older users
-								],
-							},
-						],
-					},
-				},
-			},
-			//sort based on the latest user created
-			{ $sort: { _id: -1 } },
-			{ $limit: last || first || 20 },
-		]);
+  @Authorized(["ADMIN"])
+  @Query(() => UserConnection)
+  async users(
+    @Args() { first, after, last, before }: UserArgs
+  ): Promise<UserConnection> {
+    const users = await this.userService.aggregate([
+      {
+        $match: {
+          $expr: {
+            $cond: [
+              { $and: [{ $eq: [before, null] }, { $eq: [after, null] }] },
+              { $ne: ["$_id", null] }, //return all sorted documents
+              {
+                $cond: [
+                  { $eq: [before, null] },
+                  { $lt: ["$_id", new ObjectId(after)] }, //newer users
+                  { $gt: ["$_id", new ObjectId(before)] }, //older users
+                ],
+              },
+            ],
+          },
+        },
+      },
+      //sort based on the latest user created
+      { $sort: { _id: -1 } },
+      { $limit: last || first || 20 },
+    ]);
 
-		return {
-			edges: users.map((user) => ({
-				cursor: user?._id,
-				node: transformIds(user),
-			})),
-			pageInfo: {
-				startCursor: users[0]._id,
-				hasPreviousPage:
-					(await this.userService.exists({
-						_id: { $gt: users[0]._id },
-					})) !== null,
-				endCursor: users[users.length - 1]._id,
-				hasNextPage:
-					(await this.userService.exists({
-						_id: { $lt: users[users.length - 1]._id },
-					})) !== null,
-			},
-		};
-	}
+    return {
+      edges: users.map((user) => ({
+        cursor: user?._id,
+        node: transformIds(user),
+      })),
+      pageInfo: {
+        startCursor: users[0]._id,
+        hasPreviousPage:
+          (await this.userService.exists({
+            _id: { $gt: users[0]._id },
+          })) !== null,
+        endCursor: users[users.length - 1]._id,
+        hasNextPage:
+          (await this.userService.exists({
+            _id: { $lt: users[users.length - 1]._id },
+          })) !== null,
+      },
+    };
+  }
 
-	@Authorized()
-	@Query(() => User)
-	async me(@Ctx() { user }: Context): Promise<User> {
-		const loggedInUser = await this.userService.findOne({ _id: user?.id });
-		if (!loggedInUser)
-			throw new AuthenticationError("User account has been deleted!");
+  @Authorized()
+  @Query(() => User)
+  async me(@Ctx() { user }: Context): Promise<User> {
+    const loggedInUser = await this.userService.findOne({ _id: user?.id });
+    if (!loggedInUser)
+      throw new AuthenticationError("User account has been deleted!");
 
-		return loggedInUser;
-	}
+    return loggedInUser;
+  }
 
-	@Mutation(() => User)
-	async register(
-		@Arg("data") registerInput: RegisterInput,
-		@Ctx() { res }: Context
-	) {
-		const user = await this.userService.create(registerInput);
+  @Mutation(() => User)
+  async register(
+    @Arg("data") registerInput: RegisterInput,
+    @Ctx() { res }: Context
+  ) {
+    console.log(registerInput);
+    const user = await this.userService.create(registerInput);
 
-		res.cookie("accessToken", user.token, {
-			httpOnly: true,
-			expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
-			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-			secure: process.env.NODE_ENV === "production",
-		});
+    res.cookie("accessToken", user.token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
 
-		return user;
-	}
+    return user;
+  }
 
-	@Mutation(() => User)
-	async login(
-		@Arg("email") email: string,
-		@Arg("password") password: string,
-		@Ctx() { res }: Context
-	): Promise<User> {
-		const user = await this.userService.findOne({ email });
-		if (!user) throw new AuthenticationError("Invalid credentials!");
+  @Mutation(() => User)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: Context
+  ): Promise<User> {
+    const user = await this.userService.findOne({ email });
+    if (!user) throw new AuthenticationError("Invalid credentials!");
 
-		const match = await compare(password, user.password);
-		if (!match) throw new AuthenticationError("Invalid credentials!");
+    const match = await compare(password, user.password);
+    if (!match) throw new AuthenticationError("Invalid credentials!");
 
-		res.cookie("accessToken", user.token, {
-			httpOnly: true,
-			expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
-			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-			secure: process.env.NODE_ENV === "production",
-		});
+    res.cookie("accessToken", user.token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 60 * 1000 * 24 * 7),
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
 
-		return user;
-	}
+    return user;
+  }
 
-	@Authorized()
-	@Mutation(() => Boolean)
-	logout(@Ctx() { res }: Context) {
-		res.clearCookie("accessToken");
+  @Authorized()
+  @Mutation(() => Boolean)
+  logout(@Ctx() { res }: Context) {
+    res.clearCookie("accessToken");
 
-		return true;
-	}
+    return true;
+  }
 
-	@Query(() => String)
-	async forgotPassword(@Arg("email") email: string): Promise<string> {
-		const user = await this.userService.findOne({ email });
-		if (!user)
-			throw new UserInputError("No user with provided email address found!");
+  @Query(() => String)
+  async forgotPassword(@Arg("email") email: string): Promise<string> {
+    const user = await this.userService.findOne({ email });
+    if (!user)
+      throw new UserInputError("No user with provided email address found!");
 
-		const token = signJwt({ id: user.id }, { expiresIn: "1h" });
+    const token = signJwt({ id: user.id }, { expiresIn: "1h" });
 
-		sendMail(
-			email,
-			"Password Reset",
-			`Reset password with the following token: ${token}`,
-			`<html><head></head><body><p>Dear ${user.name}</p><p>Please reset your password with the following token: ${token}</p></body></html>`,
-			[]
-		);
+    sendMail(
+      email,
+      "Password Reset",
+      `Reset password with the following token: ${token}`,
+      `<html><head></head><body><p>Dear ${user.name}</p><p>Please reset your password with the following token: ${token}</p></body></html>`,
+      []
+    );
 
-		return "Password reset link has been sent to your email!";
-	}
+    return "Password reset link has been sent to your email!";
+  }
 
-	@Mutation(() => User)
-	async passwordReset(
-		@Arg("data") { password }: PasswordInput,
-		@Ctx() { req }: Context
-	): Promise<User> {
-		const token = req.headers.resettoken;
-		const userId: ResetToken = verifyJwt(token as string);
-		if (!userId) throw new Error("Reset token expired!");
+  @Mutation(() => User)
+  async passwordReset(
+    @Arg("data") { password }: PasswordInput,
+    @Ctx() { req }: Context
+  ): Promise<User> {
+    const token = req.headers.resettoken;
+    const userId: ResetToken = verifyJwt(token as string);
+    if (!userId) throw new Error("Reset token expired!");
 
-		const user = await this.userService.findOne({ _id: userId.id });
-		if (!user) throw new Error("User not found!");
+    const user = await this.userService.findOne({ _id: userId.id });
+    if (!user) throw new Error("User not found!");
 
-		user.password = password;
+    user.password = password;
 
-		return await user.save();
-	}
+    return await user.save();
+  }
 
-	@Authorized(["ADMIN", "SUPERVISOR", "IS_OWN_USER"])
-	@Mutation(() => User)
-	async updateUser(
-		@Arg("id") id: ObjectId,
-		@Arg("data") userInput: UserInput
-	): Promise<User> {
-		const user = await this.userService.findOne({ _id: id });
-		if (!user) throw new UserInputError("User not found!");
+  @Authorized(["ADMIN", "IS_OWN_USER"])
+  @Mutation(() => User)
+  async updateUser(
+    @Arg("id") id: ObjectId,
+    @Arg("data") userInput: UserInput
+  ): Promise<User> {
+    const user = await this.userService.findOne({ _id: id });
+    if (!user) throw new UserInputError("User not found!");
 
-		for (const [key, value] of Object.entries(userInput)) {
-			user[key as keyof UserInput] = value;
-		}
+    for (const [key, value] of Object.entries(userInput)) {
+      user[key as keyof UserInput] = value;
+    }
 
-		return await user.save();
-	}
+    return await user.save();
+  }
 
-	//Mutation used to save newly added billing while registering for a conference
-	@Authorized()
-	@Mutation(() => User)
-	async updateBilling(
-		@Ctx() { user }: Context,
-		@Arg("data") billingData: BillingInput
-	): Promise<User> {
-		const loggedInUser = await this.userService.findOne({ _id: user?.id });
-		if (!loggedInUser) throw new UserInputError("User not found!");
+  //Mutation used to save newly added billing while registering for a conference
+  @Authorized(["IS_OWN_USER"])
+  @Mutation(() => User)
+  async updateBilling(
+    @Ctx() { user }: Context,
+    @Arg("data") billingData: BillingInput
+  ): Promise<User> {
+    const loggedInUser = await this.userService.findOne({ _id: user?.id });
+    if (!loggedInUser) throw new UserInputError("User not found!");
 
-		const billing = loggedInUser.billings.find(
-			(billing) => billing.name === billingData.name
-		);
-		if (billing) {
-			billing.address = billingData.address;
-			billing.DIC = billingData.DIC;
-			billing.ICO = billingData.ICO;
-			billing.ICDPH = billingData.ICDPH;
-		} else {
-			loggedInUser.billings.push(billingData);
-		}
+    const billing = loggedInUser.billings.find(
+      (billing) => billing.name === billingData.name
+    );
+    if (billing) {
+      billing.address = billingData.address;
+      billing.DIC = billingData.DIC;
+      billing.ICO = billingData.ICO;
+      billing.ICDPH = billingData.ICDPH;
+    } else {
+      loggedInUser.billings.push(billingData);
+    }
 
-		return await loggedInUser.save();
-	}
+    return await loggedInUser.save();
+  }
 
-	@Authorized(["ADMIN"])
-	@Mutation(() => Boolean)
-	async deleteUser(@Arg("id") id: ObjectId): Promise<boolean> {
-		const { deletedCount } = await this.userService.delete({ _id: id });
-		return deletedCount > 0;
-	}
+  @Authorized(["ADMIN"])
+  @Mutation(() => Boolean)
+  async deleteUser(@Arg("id") id: ObjectId): Promise<boolean> {
+    const { deletedCount } = await this.userService.delete({ _id: id });
+    return deletedCount > 0;
+  }
 }
